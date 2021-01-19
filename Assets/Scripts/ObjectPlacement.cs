738934437e21477b1bcc4c246cf4cad0b79122a8
Rahul Kaushik <rahulk1991@gmail.com>
2021-01-19 18:57:51 +0530

    -Final commit
    -Added touch to place object
    -Added sliders to change to position of the placed object
    -Optimisations

--- Assets/Scripts/ObjectPlacement.cs
+++ Assets/Scripts/ObjectPlacement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 
@@ -14,6 +15,17 @@ public class ObjectPlacement : MonoBehaviour
     [SerializeField]
     private GameObject modelParent = null;
 
+    //Sliders to control the movement of the instantiated object..
+    [SerializeField]
+    private Slider horizontalSlider = null;
+    [SerializeField]
+    private Slider verticalSlider = null;
+    private bool isSliderClicked = false;
+
+    //Getting current and new slider values to move the instantiated object..
+    float currentHSliderValue, newHSliderValue;
+    float currentVSliderValue, newVSliderValue;
+   
     //This is a reference to the object we have spawned, in case we need to use it for further modification like scaling, etc..
     private GameObject placedObject;
     private ARRaycastManager aRRaycastManager;
@@ -29,12 +41,20 @@ public class ObjectPlacement : MonoBehaviour
         aRRaycastManager = GetComponent<ARRaycastManager>();
         //Disabling multi touch as we require only one touch at a time
         Input.multiTouchEnabled = false;
+
+        //Adding listeners to the sliders
+        horizontalSlider.onValueChanged.AddListener(delegate { SliderValueChanged(0); });
+        verticalSlider.onValueChanged.AddListener(delegate { SliderValueChanged(1); });
+
+        //Getting current value of slider value
+        currentHSliderValue = horizontalSlider.value;
+        currentVSliderValue = verticalSlider.value;
     }
 
     //Method to get the current touch position
     private bool GetTouchPosition(out Vector2 tapPosition)
     {
-        if(Input.touchCount > 0)
+        if (Input.touchCount > 0)
         {
             //Number of touches is greater than zero, we are getting the current touch position on the screen
             tapPosition = Input.GetTouch(0).position;
@@ -49,27 +69,82 @@ public class ObjectPlacement : MonoBehaviour
     {
         //No touches detected..
         if (!GetTouchPosition(out Vector2 tapPosition))
-            return;
-        
-        if(aRRaycastManager.Raycast(tapPosition, _hits, TrackableType.PlaneWithinPolygon))
         {
-            //Getting current touch position from raycast
-            var _hitPose = _hits[0].pose;
-            //Setting model parent position to current touch position
-            modelParent.transform.position = _hitPose.position;
+            return;
+        }
 
-            if(placedObject == null)
+        if (aRRaycastManager.Raycast(tapPosition, _hits, TrackableType.PlaneWithinPolygon))
+        {
+            if (!isSliderClicked)
             {
-                //No objects spawned yet, instantiate the model we want to create at given position and rotation
-                placedObject = Instantiate(modelToPlace, _hitPose.position, _hitPose.rotation);
+                //Getting current touch position from raycast
+                var _hitPose = _hits[0].pose;
+                //Setting model parent position to current touch position
+                modelParent.transform.position = _hitPose.position;
+
+                if (placedObject == null)
+                {
+                    //No objects spawned yet, instantiate the model we want to create at given position and rotation
+                    placedObject = Instantiate(modelToPlace, _hitPose.position, _hitPose.rotation);
+                }
+                else
+                {
+                    //Object already exists, change position
+                    placedObject.transform.position = _hitPose.position;
+                }
+                //Setting instantiated model's parent to model parent
+                placedObject.gameObject.transform.parent = modelParent.transform;
             }
             else
             {
-                //Object already exists, change position
-                placedObject.transform.position = _hitPose.position;
+                Debug.Log("-->> Bool is false");
             }
-            //Setting instantiated model's parent to model parent
-            placedObject.gameObject.transform.parent = modelParent.transform;
         }
+    }
+
+    //Method to move the instantiated object based on the slider value..
+    private void SliderValueChanged(int value)
+    {
+        //Getting value to move the instantiated object..
+        newHSliderValue = horizontalSlider.value - currentHSliderValue;
+        newVSliderValue = verticalSlider.value - currentVSliderValue;
+        //Switching between horizontal and vertical sliders
+        switch (value)
+        {
+            case 0:
+                //Moving the model..
+                modelParent.transform.position += new Vector3((newHSliderValue), 0f, 0f);
+                //Setting the new slider value as current value
+                currentHSliderValue = horizontalSlider.value;
+                break;
+            case 1:
+                //Moving the model..
+                modelParent.transform.position += new Vector3(0f, (newVSliderValue), 0f);
+                //Setting the new slider value as current value
+                currentVSliderValue = verticalSlider.value;
+                break;
+        }
+    }
+
+    //Method to disable and enable the placement of object in Update function
+    public void OnSliderClickDetected(int value)
+    {
+        switch (value)
+        {
+            case 0:
+                isSliderClicked = true;
+                break;
+            case 1:
+                StopAllCoroutines();
+                StartCoroutine(EnableTouch());
+                break;
+        }
+    }
+
+    //Coroutine to enable touch again..
+    private IEnumerator EnableTouch()
+    {
+        yield return new WaitForEndOfFrame();
+        isSliderClicked = false;
     }
 }
